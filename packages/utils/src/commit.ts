ff0ae1dd215969daaf14518662c86586daf5da94
Chidubem Ovbiebo <chidubem@troveapp.co>
2025-01-20 16:51:45 +0100

    Added support for squashed commits

--- packages/utils/src/commit.ts
+++ packages/utils/src/commit.ts
@@ -3,14 +3,18 @@ import execAsync from "./execute";
 export async function getCommitsSince(start: string | null, matchers: string[], maxCount: number = -1) {
     const range = start === null ? "" : `${start}..HEAD`;
     const maxCountParam = maxCount > -1 ? `--max-count=${maxCount}` : "";
-    let matchersString = matchers.map(matcher => `--grep="${matcher}"`).join(" ")
-    if (matchersString) matchersString += " -E"
 
-    const command = `git log ${range} --pretty=format:"- %s" --reverse ${matchersString} --regexp-ignore-case ${maxCountParam}`;
+    let matcherStringForGit = matchers.map(matcher => `--grep="${matcher}"`).join(" ")
+    if (matcherStringForGit) matcherStringForGit += " -E"
+
+    let grepPipe = matchers.map(matcher => `-e "${matcher}"`).join(" ")
+    if (grepPipe) grepPipe = "| grep -E -i " + grepPipe;
+
+    const command = `git log ${range} --pretty=format:"%s%n%b" --reverse ${matcherStringForGit} --regexp-ignore-case ${maxCountParam} ${grepPipe}`;
     const {stdout: commits} = await execAsync(command, {
         encoding: "utf-8",
     })
-    return commits?.trim();
+    return commits.trim();
 }
 
 export async function getLastCommitSHA(branch: string) {
